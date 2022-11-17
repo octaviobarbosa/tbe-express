0a3b1e0ca281f3042f5973930737b392759c19cd
JordanVinicius <jordanvinicius@yahoo.com.br>
2022-11-16 21:46:17 -0300

    feat: add test to project

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ app.use(router);
 router.get('*', (req, res)  => res.status(404).send('404 - Not Found'));
 
 app.listen(port, () => console.log('Server listening on port 3000!')) 
+
+module.exports={app}
